c41b7b5dd268fc7a98d7a8f67b98411aa266519f
null221 <agd1923@gmail.com>
2026-04-17 20:29:52 +0200

    Cambios test themes tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,41 +11,41 @@ export default {
   theme: {
     extend: {
       colors: {
-        surface: "#131313",
-        background: "#131313",
-        onSurface: "#e2e2e2",
+        "surface": "#131313",
+        "background": "#131313",
+        "on-surface": "#e2e2e2",
 
-        primary: "#c9bfff",
-        primaryContainer: "#917eff",
-        onPrimary: "#2e009c",
-        onPrimaryContainer: "#28008a",
+        "primary": "#c9bfff",
+        "primary-container": "#917eff",
+        "on-primary": "#2e009c",
+        "on-primary-container": "#28008a",
 
-        secondary: "#ffb4a3",
-        secondaryContainer: "#783525",
-        onSecondary: "#581d0f",
-        onSecondaryContainer: "#fea08b",
+        "secondary": "#ffb4a3",
+        "secondary-container": "#783525",
+        "on-secondary": "#581d0f",
+        "on-secondary-container": "#fea08b",
 
-        tertiary: "#a4c9ff",
-        tertiaryContainer: "#4d93e5",
-        onTertiary: "#00315d",
+        "tertiary": "#a4c9ff",
+        "tertiary-container": "#4d93e5",
+        "on-tertiary": "#00315d",
 
-        error: "#ffb4ab",
-        errorContainer: "#93000a",
-        onError: "#690005",
-        onErrorContainer: "#ffdad6",
+        "error": "#ffb4ab",
+        "error-container": "#93000a",
+        "on-error": "#690005",
+        "on-error-container": "#ffdad6",
 
-        outline: "#928ea1",
-        outlineVariant: "#484555",
+        "outline": "#928ea1",
+        "outline-variant": "#484555",
 
-        surfaceVariant: "#353535",
-        surfaceBright: "#393939",
-        surfaceDim: "#131313",
+        "surface-variant": "#353535",
+        "surface-bright": "#393939",
+        "surface-dim": "#131313",
 
-        surfaceContainerLowest: "#0e0e0e",
-        surfaceContainerLow: "#1b1b1b",
-        surfaceContainer: "#1f1f1f",
-        surfaceContainerHigh: "#2a2a2a",
-        surfaceContainerHighest: "#353535",
+        "surface-container-lowest": "#0e0e0e",
+        "surface-container-low": "#1b1b1b",
+        "surface-container": "#1f1f1f",
+        "surface-container-high": "#2a2a2a",
+        "surface-container-highest": "#353535",
       },
 
       fontFamily: {
@@ -64,5 +65,3 @@ export default {
 
   plugins: [],
 };
-
-console.log("TAILWIND CONFIG LOADED");
