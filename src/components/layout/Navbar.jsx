e2da9438517f8a2b284e3ba13df622c6535de32f
null221 <agd1923@gmail.com>
2026-04-18 19:09:43 +0200

    Refactor botones navbar, add vinculo a pagina correspondiente

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Bell, BellRing, PersonStanding, User } from "lucide-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <nav className="fixed top-0 w-full z-50 bg-[#131313]/70 backdrop-blur-xl">
       <div className="flex items-center justify-between px-8 py-4 max-w-screen-2xl mx-auto">
@@ -9,17 +12,17 @@ export default function Navbar() {
         </div>
 
         <div className="hidden md:flex space-x-8">
-          <a className="font-headline text-gray-400 hover:text-white">Releases</a>
-          <a className="font-headline text-gray-400 hover:text-white">Calendar</a>
-          <a className=" font-headline text-white border-b-2 border-[#7B61FF]">
+          <button className="font-headline text-gray-400 hover:text-white" onClick={() => navigate('/releases')}>Releases</button>
+          <button className="font-headline text-gray-400 hover:text-white" onClick={() => navigate('/calendar')}>Calendar</button>
+          <button className=" font-headline text-white border-b-2 border-[#7B61FF]" onClick={() => navigate('/')}>
             Upcoming
-          </a>
+          </button>
         </div>
         <div className="flex items-center space-x-6">
           <button className="text-[#e2e2e2] hover:text-[#c9bfff] transition-colors">
             <Bell data-icon="notifications"/>
           </button>
-          <button class="text-on-surface hover:text-primary transition-colors">
+          <button className="text-on-surface hover:text-primary transition-colors">
             <span>BELL</span>
           </button>
           <button className="text-[#e2e2e2] hover:text-[#c9bfff] transition-colors">
