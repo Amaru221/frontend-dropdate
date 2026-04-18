bd2132699d3286ec0f3ffbacf948debd62c06b36
null221 <agd1923@gmail.com>
2026-04-18 19:08:25 +0200

    refactor llamadas backend con axios. Agregamos llamada getGamesReleasesRange

--- src/api/games.api.js
+++ src/api/games.api.js
@@ -1,7 +1,30 @@
-const API_URL = "https://localhost:8000/api";
+import api, { patchWithMerge } from "./http";
 
-export async function getGames() {
+
+export async function getGamesRelasesRange1() {
   const res = await fetch(`${API_URL}/games`);
   if (!res.ok) throw new Error("Error fetching games");
   return res.json();
 }
+
+
+
+export async function getGames() {
+    const response = await api.get(`/games`);
+    console.log(response);
+    return response.data;
+}
+
+export async function getGamesRelasesRange(startDate, endDate, platform) {
+
+  const params = {
+    startDate: startDate|| undefined,
+    endDate: endDate || undefined,
+    platform: platform || undefined,
+  };
+
+  const response = await api.get(`/games/search`, {params});
+  return response.data;
+}
+
+
